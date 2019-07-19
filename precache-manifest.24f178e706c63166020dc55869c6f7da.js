ea3aaa91be653913d42495d267ea9d63f398dde3
Agam Rafaeli <agam.rafaeli@gmail.com>
2019-07-19 13:25:09 +0300

    Updates

---
+++ precache-manifest.24f178e706c63166020dc55869c6f7da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6ee6cf56de910c7590831b5891f798",
+    "url": "/anyway-schools-report/index.html"
+  },
+  {
+    "revision": "88e492950d6626fb873d",
+    "url": "/anyway-schools-report/static/css/main.86cfbdf1.chunk.css"
+  },
+  {
+    "revision": "f34b9bd50d3b9f37d4f6",
+    "url": "/anyway-schools-report/static/js/2.f430d975.chunk.js"
+  },
+  {
+    "revision": "88e492950d6626fb873d",
+    "url": "/anyway-schools-report/static/js/main.ab3c8c67.chunk.js"
+  },
+  {
+    "revision": "dbf1d83ec3bc17e6e1d1",
+    "url": "/anyway-schools-report/static/js/runtime~main.c5ff331e.js"
+  }
+]);
