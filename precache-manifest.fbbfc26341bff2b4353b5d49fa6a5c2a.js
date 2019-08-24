de988231e47ded357f5f55d1d6fbdafb357a1718
Agam Rafaeli <agam.rafaeli@gmail.com>
2019-08-24 20:45:50 +0300

    Updates

---
+++ precache-manifest.fbbfc26341bff2b4353b5d49fa6a5c2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06e29e35f8b3f993f4d7e073869b37f",
+    "url": "/anyway-schools-report/index.html"
+  },
+  {
+    "revision": "59e6fcd337b38e3f8be9",
+    "url": "/anyway-schools-report/static/css/main.9c56b2a2.chunk.css"
+  },
+  {
+    "revision": "ce2c4b396dce316daea2",
+    "url": "/anyway-schools-report/static/js/2.bba8163f.chunk.js"
+  },
+  {
+    "revision": "59e6fcd337b38e3f8be9",
+    "url": "/anyway-schools-report/static/js/main.4003a520.chunk.js"
+  },
+  {
+    "revision": "dbf1d83ec3bc17e6e1d1",
+    "url": "/anyway-schools-report/static/js/runtime~main.c5ff331e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anyway-schools-report/precache-manifest.76d0ee369ecadb9733741a06af25510e.js"
+  "/anyway-schools-report/precache-manifest.fbbfc26341bff2b4353b5d49fa6a5c2a.js"
 );
 
 self.addEventListener('message', (event) => {
