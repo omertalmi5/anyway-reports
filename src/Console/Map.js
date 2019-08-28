5a45d958bc929b5e93a0680f872ea13e6b9424f6
Avi <aviklaiman@hotmail.com>
2019-08-28 22:06:23 +0300

    Hide search only on simple view

--- src/Console/Map.js
+++ src/Console/Map.js
@@ -5,9 +5,9 @@ import './Map.scss';
 function getLink(school, simpleView) {
     let lat = school.latitude;
     let long = school.longitude;
-    let link = `https://www.anyway.co.il/?zoom=17&lat=${lat}&lon=${long}&start_date=2014-01-01&end_date=2018-12-31&show_fatal=1&show_severe=1&show_light=1&approx=&accurate=1&show_markers=1&show_discussions=0&show_urban=3&show_intersection=3&show_lane=3&show_day=7&show_holiday=0&show_time=24&start_time=25&end_time=25&weather=0&road=0&separation=0&surface=0&acctype=1&controlmeasure=0&district=0&case_type=0&show_rsa=0&age_groups=1234&hide_search=true`;
+    let link = `https://www.anyway.co.il/?zoom=17&lat=${lat}&lon=${long}&start_date=2014-01-01&end_date=2018-12-31&show_fatal=1&show_severe=1&show_light=1&approx=&accurate=1&show_markers=1&show_discussions=0&show_urban=3&show_intersection=3&show_lane=3&show_day=7&show_holiday=0&show_time=24&start_time=25&end_time=25&weather=0&road=0&separation=0&surface=0&acctype=1&controlmeasure=0&district=0&case_type=0&show_rsa=0&age_groups=1234`;
     if (simpleView) {
-        link = `${link}&map_only=true`;
+        link = `${link}&map_only=true&hide_search=true`;
     }
     return link;
 }
