22ae6ed4ca7ee3617baa5f0676bc17fdd727010f
sa1omon <salomon@gamoshi.com>
2020-04-12 13:21:27 +0300

    Updates

---
+++ precache-manifest.ac910d0306bed136414196c4932ce663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194d5dabef4dd4e50667fcad98ddf5c5",
+    "url": "/anyway-schools-report/index.html"
+  },
+  {
+    "revision": "dcb688da2393d53c56b4",
+    "url": "/anyway-schools-report/static/css/main.3c4efd4b.chunk.css"
+  },
+  {
+    "revision": "fc4b6777b713e766f78a",
+    "url": "/anyway-schools-report/static/js/2.74b34121.chunk.js"
+  },
+  {
+    "revision": "dcb688da2393d53c56b4",
+    "url": "/anyway-schools-report/static/js/main.d48a1446.chunk.js"
+  },
+  {
+    "revision": "dbf1d83ec3bc17e6e1d1",
+    "url": "/anyway-schools-report/static/js/runtime~main.c5ff331e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anyway-schools-report/precache-manifest.b3ba85b0a30b26471deb747ab84528a0.js"
+  "/anyway-schools-report/precache-manifest.ac910d0306bed136414196c4932ce663.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anyway-schools-report/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
